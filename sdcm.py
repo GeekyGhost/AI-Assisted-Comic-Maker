7d196a211f7f1de840c71c4855c79cc7f9d1677f
GeekyGhost <111990299+GeekyGhost@users.noreply.github.com>
2023-04-21 07:12:58 -0400

    Update sdcm.py

--- sdcm.py
+++ sdcm.py
@@ -20,60 +20,6 @@ def inpaint_image():
 def outpaint_image():
     # Code to outpaint the selected panels
 
-def create_primitive():
-    # Code to create a primitive shape
-
-menu = tk.Menu(root)
-root.config(menu=menu)
-
-file_menu = tk.Menu(menu)
-menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Open", command=open_image)
-file_menu.add_command(label="Save", command=save_image)
-file_menu.add_separator()
-file_menu.add_command(label="Exit", command=root.quit)
-
-canvas = tk.Canvas(root, width=600, height=400, bg="white")
-canvas.pack(side="left", padx=10, pady=10)
-
-tools_frame = tk.Frame(root)
-tools_frame.pack(side="right", padx=10, pady=10)
-
-generate_button = tk.Button(tools_frame, text="Generate", command=generate_image)
-generate_button.pack(fill="both", padx=5, pady=5)
-
-inpaint_button = tk.Button(tools_frame, text="Inpaint", command=inpaint_image)
-inpaint_button.pack(fill="both", padx=5, pady=5)
-
-outpaint_button = tk.Button(tools_frame, text="Outpaint", command=outpaint_image)
-outpaint_button.pack(fill="both", padx=5, pady=5)
-
-primitive_button = tk.Button(tools_frame, text="Create Primitive", command=create_primitive)
-primitive_button.pack(fill="both", padx=5, pady=5)
-
-root.mainloop()
-import tkinter as tk
-from tkinter import filedialog, messagebox
-
-root = tk.Tk()
-root.title("Comic Book Creator")
-root.geometry("800x600")
-
-def open_image():
-    # Code to open an image and display it on the canvas
-
-def save_image():
-    # Code to save the current canvas as an image
-
-def generate_image():
-    # Code to generate an image using the AI art generator
-
-def inpaint_image():
-    # Code to inpaint the selected panel
-
-def outpaint_image():
-    # Code to outpaint the selected panels
-
 class ResizableShape:
     def __init__(self, canvas, shape_type="rectangle", x=0, y=0, width=100, height=100, color="black"):
         self.canvas = canvas
@@ -150,3 +96,4 @@ primitive_button = tk.Button(tools_frame, text="Create Primitive", command=creat
 primitive_button.pack(fill="both", padx=5, pady=5)
 
 root.mainloop()
+

5bf6ff86c2a6faff4eb1a2c6e8f93c2b69932128
GeekyGhost <111990299+GeekyGhost@users.noreply.github.com>
2023-04-21 07:11:51 -0400

    Update sdcm.py

--- sdcm.py
+++ sdcm.py
@@ -52,3 +52,101 @@ primitive_button = tk.Button(tools_frame, text="Create Primitive", command=creat
 primitive_button.pack(fill="both", padx=5, pady=5)
 
 root.mainloop()
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+root = tk.Tk()
+root.title("Comic Book Creator")
+root.geometry("800x600")
+
+def open_image():
+    # Code to open an image and display it on the canvas
+
+def save_image():
+    # Code to save the current canvas as an image
+
+def generate_image():
+    # Code to generate an image using the AI art generator
+
+def inpaint_image():
+    # Code to inpaint the selected panel
+
+def outpaint_image():
+    # Code to outpaint the selected panels
+
+class ResizableShape:
+    def __init__(self, canvas, shape_type="rectangle", x=0, y=0, width=100, height=100, color="black"):
+        self.canvas = canvas
+        self.shape_type = shape_type
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.shape_id = None
+        self.selected = False
+
+        self.create_shape()
+
+    def create_shape(self):
+        if self.shape_type == "rectangle":
+            self.shape_id = self.canvas.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, outline=self.color)
+        elif self.shape_type == "oval":
+            self.shape_id = self.canvas.create_oval(self.x, self.y, self.x + self.width, self.y + self.height, outline=self.color)
+        # Add more shapes here as needed
+
+        self.canvas.tag_bind(self.shape_id, "<ButtonPress-1>", self.on_shape_press)
+        self.canvas.tag_bind(self.shape_id, "<B1-Motion>", self.on_shape_drag)
+        self.canvas.tag_bind(self.shape_id, "<ButtonRelease-1>", self.on_shape_release)
+
+    def on_shape_press(self, event):
+        self.selected = True
+        self.canvas.itemconfig(self.shape_id, outline="red")
+
+    def on_shape_drag(self, event):
+        if self.selected:
+            self.canvas.move(self.shape_id, event.x - self.x, event.y - self.y)
+            self.x, self.y = event.x, event.y
+
+    def on_shape_release(self, event):
+        self.selected = False
+        self.canvas.itemconfig(self.shape_id, outline=self.color)
+
+def create_primitive():
+    shape_type = "rectangle"  # You can create a dropdown menu to let the user choose the shape type
+    color = colorchooser.askcolor()[1]
+    if color is None:
+        color = "black"
+
+    shape = ResizableShape(canvas, shape_type=shape_type, x=50, y=50, width=100, height=100, color=color)
+
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open", command=open_image)
+file_menu.add_command(label="Save", command=save_image)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+canvas = tk.Canvas(root, width=600, height=400, bg="white")
+canvas.pack(side="left", padx=10, pady=10)
+
+tools_frame = tk.Frame(root)
+tools_frame.pack(side="right", padx=10, pady=10)
+
+generate_button = tk.Button(tools_frame, text="Generate", command=generate_image)
+generate_button.pack(fill="both", padx=5, pady=5)
+
+inpaint_button = tk.Button(tools_frame, text="Inpaint", command=inpaint_image)
+inpaint_button.pack(fill="both", padx=5, pady=5)
+
+outpaint_button = tk.Button(tools_frame, text="Outpaint", command=outpaint_image)
+outpaint_button.pack(fill="both", padx=5, pady=5)
+
+primitive_button = tk.Button(tools_frame, text="Create Primitive", command=create_primitive)
+primitive_button.pack(fill="both", padx=5, pady=5)
+
+root.mainloop()
